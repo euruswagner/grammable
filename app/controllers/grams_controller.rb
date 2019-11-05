ec3ef131848af070fd888c76429887da97c000a1
Eurus Wagner <euruswagner@gmail.com>
2019-11-05 15:13:05 +0000

    Added create to gram controller

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -3,6 +3,22 @@ class GramsController < ApplicationController
     @gram = Gram.new
   end
 
+  def create
+    @gram = Gram.create(gram_params)
+    if @gram.valid? then
+      redirect_to root_path
+    else
+      return render :new, status: :unprocessable_entity
+    end
+  end
+
   def index
   end
+
+  private
+
+  def gram_params
+    params.require(:gram).permit(:message)
+  end
+
 end
