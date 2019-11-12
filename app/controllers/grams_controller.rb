4f947b1a3c3419d09a1e9e50da549f22ec706734
Eurus Wagner <euruswagner@gmail.com>
2019-11-12 19:55:26 +0000

    Installed Carrierwave

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -17,6 +17,7 @@ class GramsController < ApplicationController
   end
 
   def index
+    @grams = Gram.all
   end
 
   def edit
@@ -47,7 +48,7 @@ class GramsController < ApplicationController
   private
 
   def gram_params
-    params.require(:gram).permit(:message)
+    params.require(:gram).permit(:message, :picture)
   end
 
   def render_not_found(status=:not_found)
